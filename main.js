2062d5741b4d5cc0ea1c48a57fef1ae404db89aa
YuukaDev <76272667+YuukaDev@users.noreply.github.com>
2021-05-22 01:02:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const client = new Discord.Client({
 // Command Handler && Welcome Command && Distube
 const CommandHandler = require("wokcommands");
 const welcome = require('./commands/others/welcome');
-const distube = new DisTube(client, {searchSongs: false, emitNewSongOnly: false, });
+const distube = new DisTube(client, {searchSongs: false, emitNewSongOnly: true, });
 
 client.once('ready', () => {
     console.log('Bot je uključen');
@@ -41,75 +41,91 @@ client.on("message", async (message) => {
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift();
     const string = args.join(" ");
+    ``
 
     if(command == 'play') {
         if(!message.member.voice.channel) {
-            return message.channel.send('❌You must be in a voice channel to use this command');
-        }    
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
         distube.play(message, string);
     }
 
     if (command == "skip") {
-        distube.skip(message, string);
+        if(!message.member.voice.channel) {
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
+        let queue = await distube.getQueue(message);
+        if(queue) {
+            distube.skip(message)
+        } else if (!queue) {
+            return;
+        };
     }
 
-    if (command === 'queue') {
-		const queue = distube.getQueue(message)
-		message.channel.send(`Current queue:\n${queue.songs.map((song, id) =>
-			`**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``).slice(0, 10).join('\n')}`)
-	}
-
     if(command == 'pause') {
+        if(!message.member.voice.channel) {
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
         distube.pause(message, string);
+        return message.channel.send('⏸️ | Song is now paused | 🎵');
     }
 
     if(command == 'resume') {
+        if(!message.member.voice.channel) {
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
         distube.resume(message, string);
+        return message.channel.send('▶️ | Song is played again | 🎵');
     }
 
     if (command == "loop") {
+        if(!message.member.voice.channel) {
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
         distube.setRepeatMode(message, parseInt(args[0]));
+        return message.channel.send('🔁 | Loop is now enabled | 🎵');
     }
+
+    if (command === 'queue') {
+        if(!message.member.voice.channel) {
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
+		const queue = distube.getQueue(message)
+		message.channel.send(`🎵 | Music Queue | 🎵\n${queue.songs.map((song, id) =>
+			`**${id + 1}**. \`${song.name} - ${song.formattedDuration}\``).slice(0, 10).join('\n')}`)
+
+        if(queue === null || queue === undefined) {
+            message.channel.send(`Radi \n${queue.songs.map((song, id) =>
+                `**${id + 1}**. \`${song.name} - ${song.formattedDuration}\``).slice(0, 10).join('\n')}`)
+        }
+	}
 
     if (command == "leave") {
+        if(!message.member.voice.channel) {
+            return message.channel.send('❌ | You must be in a voice channel to use this command');
+        }
         distube.stop(message, string);
-        message.channel.send("Leave the channel");
-    }
-
-    if ([`3d`, `bassboost`, `echo`, `karaoke`, `nightcore`, `vaporwave`].includes(command)) {
-        let filter = distube.setFilter(message, command);
-        message.channel.send("Current queue filter: " + (filter || "Off"));
+        message.channel.send('📃 | Player is now leaving | 🎵');
     }
 });
 
 // Queue status template
 const status = (queue) => `"All Queue" : "This Song" : "Off"}\``
 
-// DisTube event listeners, more in the documentation page
+// Distube Event Listener
 distube
     .on("playSong", (message, queue, song) => message.channel.send(
-        `✅ Song \`${song.name}\` has been added to queue`
+        `▶️ | Song \`${song.name}\` is now playing | 🎵`
     ))
     .on("addSong", (message, queue, song) => message.channel.send(
-        `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+        `✅ | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user} | 🎵`
     ))
-    .on("playList", (message, queue, playlist, song) => message.channel.send(
-        `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
-    ))
-    .on("addList", (message, queue, playlist) => message.channel.send(
-        `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
-    ))
-    // DisTubeOptions.searchSongs = true
-    .on("searchResult", (message, result) => {
-        let i = 0;
-        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
-    })
-    // DisTubeOptions.searchSongs = true
-    .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
-    .on("error", (message, e) => {
-        console.error(e)
-        message.channel.send("An error encountered: " + e);
+    .on("empty", message => message.channel.send("🔒 | Channel is empty, Leaving the channel | 🔒"))
+    .on('initQueue', queue => {
+        queue.autoplay = false;
+        queue.volume = 100;
     });
+    
 
 
 // Your Token
