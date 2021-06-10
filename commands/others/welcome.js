e2b578890d1bf4920203619c15afde60ba25870c
YuukaDev <vule.amv1@gmail.com>
2021-06-10 18:49:00 +0200

    Updated Welcome.js

--- commands/others/welcome.js
+++ commands/others/welcome.js
@@ -1,9 +1,10 @@
-module.exports = (client) => {
-    const channelId = "841358313983049738";
-    client.on("guildMemberAdd", (member) => {
-        const message = `Welcome <@${member.id}> to our server!`;
+/* This command sends welcome message to the member that joins in server*/
 
-  
+module.exports = (client) => {
+    const channelId = "841358313983049738"; // Channel ID
+    client.on("guildMemberAdd", (member) => {
+        const message = `Welcome <@${member.id}> to our server!`; // Welcome Message
+
         const channel = member.guild.channels.cache.get(channelId);
         channel.send(message);
     });
