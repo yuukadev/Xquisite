308a4b9936bcf3d502c1282d9638613b90b216b8
YuukaDev <vule.amv1@gmail.com>
2021-06-10 19:11:18 +0200

    Update removerole.js

--- commands/moderation/removerole.js
+++ commands/moderation/removerole.js
@@ -10,18 +10,18 @@ module.exports = {
         const member = message.mentions.members.first()
 
         if(!member) {
-            return message.channel.send(`❌ | Please specify someone`);
+            return message.channel.send(`❌ | Please specify someone`); // Message when no one is specified
         }
         
         let role = message.mentions.roles.first()
 
         if(!role) {
-            return message.channel.send(`❌ | There is no role with that name `);
+            return message.channel.send(`❌ | There is no role with that name `); // Message when there is no role
         }
 
         if(role) {
             member.roles.remove(role);
-            message.channel.send(`Role ${role} has been removed from ${member}`);
+            message.channel.send(`Role ${role} has been removed from ${member}`); // Message when role is removed
         }
     
     }
