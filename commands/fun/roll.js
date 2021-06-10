ec18ce21344165cc391f894fa4f578eeadb68fdb
YuukaDev <vule.amv1@gmail.com>
2021-06-10 19:16:40 +0200

    Created roll.js

---
+++ commands/fun/roll.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'roll',
+    description: 'this command roll a dice',
+    category: 'fun',
+    example: ['!roll'],
+    callback({message}) {
+        const choice = [1,2,3,4,5,6]; // Array of choices
+        const randomChoice = choice[Math.floor(Math.random() * choice.length)];
+        let embed = new Discord.MessageEmbed()
+        .setColor('#FF00A6')
+        .setDescription(`:game_die: You got ${randomChoice}!`);
+        message.channel.send(embed);
+    }
+}
