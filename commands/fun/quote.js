3ca86772bc4aa4e403a0148d8c8b86638e081f57
YuukaDev <vule.amv1@gmail.com>
2021-06-10 19:11:12 +0200

    Update quote.js

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -6,7 +6,7 @@ module.exports = {
     category: 'fun',
     example: ['!quote'],
     callback({message}) {
-        const quotes = [
+        const quotes = [ // All the quotes
         "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela", 
         "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. - Steve Jobs", 
         "Life is what happens when you're busy making other plans. - John Lennon", 
