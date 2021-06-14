b34d39650b85f6bdf96574f446093ee38b486b30
YuukaDev <vule.amv1@gmail.com>
2021-06-14 17:08:59 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -16,6 +16,7 @@ module.exports = {
 */ 
         .addField('Moderation Commands', ['\`!ban - Ban member\`','\`!kick - Kick member\`','\`!mute - Mute member\`','\`!unmute - Unmute member\`','\`!clear - Clear messages\`', '\`!addrole - Addrole for member\`', '\`!removerole - Remove role\`'], false)
         .addField("Fun Commands", ['\`!meme - Sends meme\`', '\`!quote - Sends quote\`', '\`!coinflip - Play game coinflip\`', '\`!reverse - Reverse a word\`', '\`!roll - Roll a dice\`', '\`!8ball - Plays 8ball game\`', '\`!howgay - How much are you gay\`'], false)
+        .addField("Anime Commands", ['\`!anime - Search anime\`', '\`!manga - Search manga\`', '\`!ranime - Sends random image of anime\`'], false)
         .addField("Action Commands", ['\`!hug - Sends gif of hugging\`', '\`!punch - Sends gif of punching\`', '\`!slap - Sends gif of slaping\`', '\`!pat - Sends gif of patting\`', '\`!pout - Sends gif of pouting\`', '\`!blush - Sends gif of blushing\`'], false)
         .addField("Info Commands", ['\`!help - Sends help embed about commands\`', '\`!info - Sends info about bot\`', '\`!avatar - Sends avatar\`',], false)
         .addField("Music Commands", ['\`!play - Plays a song\`', '\`!skip - Skips a song\`', '\`!pause - Pause a song\`', '\`!resume - Resume a paused song\`', '\`!loop - Toggles loop\`', '\`!unloop - Disable loop\`', '\`!jump - Jump to the song number in the queue\`', '\`!queue - Shows the queue of songs\`', '\`!leave - Disconnect a player\`'], false)
