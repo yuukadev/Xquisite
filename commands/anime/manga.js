c04acc0bd83902746d90102343c06fca42e646dc
YuukaDev <vule.amv1@gmail.com>
2021-06-16 17:06:15 +0200

    Update manga.js

--- commands/anime/manga.js
+++ commands/anime/manga.js
@@ -13,19 +13,19 @@ module.exports = {
 
     mal.search("manga", args, 'page').then(info => {
     const embed = new Discord.MessageEmbed() //Ver 11.5.1 of Discord.js  
-    .setAuthor(`📜 Manga for ${member.username}`)
-    .setThumbnail(`${info.results[0].image_url}`)
-    .setColor('#FF00A6')
-    .setTitle(`${info.results[0].title}`)
-    .setURL(`${info.results[0].url}`)
-    .setDescription(`${info.results[0].synopsis}`)
-    .addFields(
-    { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
-    { name: '💬 Rating', value: `${info.results[0].rated}`, inline: true},
-    { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
-    )
-    .setImage(`${info.results[0].image_url}`)
-    message.channel.send(embed);
+        .setAuthor(`📜 Manga for ${member.username}`)
+        .setThumbnail(`${info.results[0].image_url}`)
+        .setColor('#FF00A6')
+        .setTitle(`${info.results[0].title}`)
+        .setURL(`${info.results[0].url}`)
+        .setDescription(`${info.results[0].synopsis}`)
+        .addFields(
+            { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
+            { name: '💬 Rating', value: `${info.results[0].rated}`, inline: true},
+            { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
+        )
+        .setImage(`${info.results[0].image_url}`)
+        message.channel.send(embed);
             });
     }
 }
