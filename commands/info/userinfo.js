a5b50c30217f127915937c92fbcdeaa9c2b948eb
YuukaDev <vule.amv1@gmail.com>
2021-06-18 20:25:25 +0200

    Update userinfo.js

---
+++ commands/info/userinfo.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const moment = require('moment');
+module.exports = {
+    callback({message}) {
+        const user = message.mentions.users.first() || message.author;
+        const member = message.guild.members.cache.get(user.id);
+
+        let embed = new Discord.MessageEmbed()
+        .setTitle(`User Information - ${user.username}`)
+        .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+        .setColor('#FF00A6')
+        .addField(":busts_in_silhouette: Informations", [
+            `• **Name**: ${user}`,
+            `• **User ID**: ${user.id}`,
+            `• **Nickname**: ${member.nickname || 'None'}`,
+            `• **Is Bot**: ${user.bot}`,
+            `• **Created At**: ${moment(member.user.createdTimestamp).format('LL LTS')}`,
+            `• **Joined At**: ${moment(member.joinedAt).format('LL LTS')}`,
+        ])
+        message.channel.send(embed)
+    }
+}
