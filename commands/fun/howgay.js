147fe32d2a49b4d60d7b8eb2317a8cfae0734852
YuukaDev <vule.amv1@gmail.com>
2021-06-24 19:18:56 +0200

    Update howgay.js

--- commands/fun/howgay.js
+++ commands/fun/howgay.js
@@ -2,21 +2,23 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'howgay',
+    aliases: 'gay',
+    description: "This command sends how much you are gay",
     category: 'fun',
-    example: ['!howgay @member'],
+    example: ['!howgay'],
     callback({message}) {
         let member = message.mentions.users.first();
 
         let random = Math.floor(Math.random() * 101);
 
         if(!member) {
-            message.reply('Please specify someone');
+            message.channel.send('❌ | Please specify someone');
         }
 
         if(member){
             const embed = new Discord.MessageEmbed()
             .setTitle('Results')
-            .setDescription(`${member.username} is ${random}% gay :rainbow:`) // Message
+            .setDescription(`${member.username} is ${random}% gay :rainbow:`)
             .setColor('#FF00A6')
     
             message.channel.send(embed);
