b46f38acd142d085171da32ae5a305b583251e59
YuukaDev <vule.amv1@gmail.com>
2021-06-24 19:21:03 +0200

    Update anime.js

--- commands/anime/anime.js
+++ commands/anime/anime.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Jikan = require('jikan-node');
 const mal = new Jikan();
-/*
+
 let id = {
     id: [
         { Action:  1},
@@ -44,43 +44,40 @@ let id = {
         { Thriller: 45},
     ]
 }
-*/
+
 
 module.exports = {
     name: 'anime',
-    aliases: 'searchanime',
-    description: 'this command search for anime you type',
+    aliases: 'animesearch',
+    description: "This command search for anime you typed",
     category: 'anime',
-    example: ['!anime', /* Or */ '!searchanime'],
+    example: ['!anime @animename'],
     callback({message, args}) {
-    
     let member = message.author;
     
     if(!args[0]) {
-        return message.reply('Please tell me what anime to search');
+        return message.channel.send('❓ | Please tell me what anime to search');
     }
 
     mal.search("anime", args, 'page').then(info => {
     const embed = new Discord.MessageEmbed()    
-        .setAuthor(`📜 Anime for ${member.username}`)
-        .setThumbnail(`${info.results[0].image_url}`)
-        .setColor('#FF00A6')
-        .setTitle(`${info.results[0].title}`)
-        .setURL(`${info.results[0].url}`)
-        .setDescription(`${info.results[0].synopsis}`)
-        .addFields(
-            { name: ':calendar: Release Date', value: `${info.results[0].start_date}`, inline: true}, 
-            { name: ':calendar_spiral: End Date', value: `${info.results[0].end_date}`, inline: true},
-            { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
-            { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
-            { name: '💬 Rating', value: `${info.results[0].rated}`, inline: true},
-            { name: '🔩 Episodes', value: `${info.results[0].episodes}`, inline: true},
-            )
-        .setImage(`${info.results[0].image_url}`)
-        .setTimestamp()
-        message.channel.send(embed);
-            });
+    .setAuthor(`📜 Anime for ${member.username}`)
+    .setThumbnail(`${info.results[0].image_url}`)
+    .setColor('#FF00A6')
+    .setTitle(`${info.results[0].title}`)
+    .setURL(`${info.results[0].url}`)
+    .setDescription(`${info.results[0].synopsis}`)
+    .addFields(
+        { name: ':calendar: Release Date', value: `${info.results[0].start_date}`, inline: true}, 
+        { name: ':calendar_spiral: End Date', value: `${info.results[0].end_date || 'Airing'}`, inline: true},
+        { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
+        { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
+        { name: '💬 Rating', value: `${info.results[0].rated}`, inline: true},
+        { name: '🔩 Episodes', value: `${info.results[0].episodes}`, inline: true},
+    )
+    .setImage(`${info.results[0].image_url}`)
+    .setTimestamp()
+    message.channel.send(embed);
+        });
     }
 }
-
-
