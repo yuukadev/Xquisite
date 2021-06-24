24f0a00db0eca2eb39a272f3cef61089eef2c9c0
YuukaDev <vule.amv1@gmail.com>
2021-06-24 19:21:15 +0200

    Update manga.js

--- commands/anime/manga.js
+++ commands/anime/manga.js
@@ -4,33 +4,34 @@ const mal = new Jikan();
 
 module.exports = {
     name: 'manga',
-    aliases: 'searchmanga',
-    description: 'this command search for manga you type',
+    aliases: 'mangasearch',
+    description: "This command search for manga you typed",
     category: 'anime',
-    example: ['!manga', /* Or */ '!searchmanga'],
+    example: ['!manga @manganame'],
     callback({message, args}) {
     
     let member = message.author;
     
     if(!args[0]) {
-        return message.reply('Please tell me what manga to search');
+        return message.channel.send('❓ | Please tell me what manga to search');
     }
 
     mal.search("manga", args, 'page').then(info => {
-    const embed = new Discord.MessageEmbed() //Ver 11.5.1 of Discord.js  
-        .setAuthor(`📜 Manga for ${member.username}`)
-        .setThumbnail(`${info.results[0].image_url}`)
-        .setColor('#FF00A6')
-        .setTitle(`${info.results[0].title}`)
-        .setURL(`${info.results[0].url}`)
-        .setDescription(`${info.results[0].synopsis}`)
-        .addFields(
-            { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
-            { name: '💬 Rating', value: `${info.results[0].rated}`, inline: true},
-            { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
-        )
-        .setImage(`${info.results[0].image_url}`)
-        message.channel.send(embed);
-            });
+    const embed = new Discord.MessageEmbed() 
+    .setAuthor(`📜 Manga for ${member.username}`)
+    .setThumbnail(`${info.results[0].image_url}`)
+    .setColor('#FF00A6')
+    .setTitle(`${info.results[0].title}`)
+    .setURL(`${info.results[0].url}`)
+    .setDescription(`${info.results[0].synopsis}`)
+    .addFields(
+        { name: ':calendar: Release Date', value: `${info.results[0].start_date}`, inline: true}, 
+        { name: ':calendar_spiral: End Date', value: `${info.results[0].end_date || 'Airing'}`, inline: true},
+        { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
+        { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
+    )
+    .setImage(`${info.results[0].image_url}`)
+    message.channel.send(embed);
+        });
     }
 }
