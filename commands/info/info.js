3f52d1bdcb0622bafe2b104c14df4e9214ad9178
YuukaDev <vule.amv1@gmail.com>
2021-06-24 19:16:47 +0200

    Update info.js

--- commands/info/info.js
+++ commands/info/info.js
@@ -1,20 +1,21 @@
 const Discord = require('discord.js');
 module.exports = {
     name: 'info',
-    description: "this command sends info about bot",
+    description: 'This command sends info about bot',
     category: 'info',
     example: ['!info'],
-    callback: (message) => {
+    callback({message}) {
+
     const embed = new Discord.MessageEmbed()
     .setColor('#FF00A6')
     .setTitle('Xquisite Bot')
-    .setURL('https://github.com/YuukaDev')
-    .setAuthor('Yuuka', 'attachment://logo.jpg', 'https://github.com/YuukaDev')
+    .setURL('https://github.com/YuukaDev/xquisite-discordbot')
+    .setAuthor('Yuuka', 'attachment://logo.jpg')
     .setDescription("Xquisite is a friendly bot, made purely out of boredom, the goal of this bot is to improve someone's server experience, for more information look at - !help") 
     .addFields(
-	{ name: '</Twitter>', value: '[Twitter Profile](https://twitter.com/yuukasuoh)', inline: true },
-	{ name: '</GitHub>', value: '[GitHub Profile](https://twitter.com/yuukasuoh)', inline: true },
-    { name: '</Reddit>', value: '[Reddit Profile](https://www.reddit.com/user/Rintarrou)', inline: true },
+		{ name: '</Twitter>', value: '[Twitter Profile](https://twitter.com/yuukasuoh)', inline: true },
+		{ name: '</GitHub>', value: '[GitHub Profile](https://github.com/YuukaDev)', inline: true },
+        { name: '</Reddit>', value: '[Reddit Profile](https://www.reddit.com/user/Rintarrou)', inline: true },
     )
     .attachFiles('logo.jpg')
     .setImage('attachment://logo.jpg')
