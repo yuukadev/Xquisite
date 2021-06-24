284a25152a469e9650469a4d5a05cfe0803c9cea
YuukaDev <vule.amv1@gmail.com>
2021-06-24 19:25:45 +0200

    Update welcome.js

--- commands/others/welcome.js
+++ commands/others/welcome.js
@@ -1,11 +1,14 @@
-/* This command sends welcome message to the member that joins in server*/
+// This commands sends welcome message for your server
 
+
+/* Remove the comment if you want this command to work
 module.exports = (client) => {
-    const channelId = "841358313983049738"; // Channel ID
+    const channelId = ""; <==== Channel ID goes here
     client.on("guildMemberAdd", (member) => {
-        const message = `Welcome <@${member.id}> to our server!`; // Welcome Message
-
+        const message = `Welcome <@${member.id}> to our server!`; <==== This is a message that bot sends when a user join's a server
+  
         const channel = member.guild.channels.cache.get(channelId);
         channel.send(message);
     });
 };
+*/
