2b424155321b1ccd3b5f695344aabf028f9e4e63
YuukaDev <vule.amv1@gmail.com>
2021-06-24 19:13:57 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,24 +1,30 @@
 module.exports = {
     name: 'clear',
-    description: "this command clear messages",
+    description: "This command clear's chat",
     category: 'moderation',
     example: ['!clear @number'],
-    minArgs: 1,
-    requiredPermissions: ["MANAGE_MESSAGES"],
-    syntaxError: "Enter the amount of messages to clear",
-    callback({message, args}) { 
+    callback({message, args}) {
+        const permission = message.member.hasPermission("MANAGE_MESSAGES");
+
+        if(!permission) {
+            return message.channel.send("❌ | You don't have permission to use this command");
+        }
+
+        if(!args[0]) {
+            return message.channel.send('📃 | Enter the amount of messages to clear');
+        }
         if(isNaN(args[0])) {
-            return message.channel.send("❌ | Enter a real number"); // Message when is entered text instead of number
+            return message.channel.send("❌ | Enter a real number");
         }
         if(args[0] > 100) {
-            return message.channel.send("❌ | You cannot delete more than 100 messages"); // Message when you sends more than 100
+            return message.channel.send("❌ | You cannot delete more than 100 messages");
         }
         if(args[0] < 1) {
-            return message.channel.send("❌ | You have to delete at least one message"); // Message if you enter number that is less than 1
+            return message.channel.send("❌ | You have to delete at least one message");
         }
         
         message.channel.messages.fetch({ limit: args[0]}).then(messages =>{
         message.channel.bulkDelete(messages)
-    });
- }
+        });
+    }
 } 
