5c50655e6ca0ed8b5486c14e9323bb414f0adcf7
YuukaDev <vule.amv1@gmail.com>
2021-10-19 03:12:10 +0200

    Create panda.js

---
+++ commands/fun/panda.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'panda',
+    description: "This command sends you random panda image",
+    category: 'fun',
+    example: ['!panda'],
+    callback({message}) {
+        const pandaFunction = async () => {
+            let member = message.author;
+            const res = await fetch("https://some-random-api.ml/img/panda");
+            const random = await res.json();
+
+            let replies = [
+                "here's some picture for",
+                "awwww look at that",
+                "thats cute!"
+            ]
+
+            const randomReplies = replies[Math.floor(Math.random() * replies.length)];
+
+            let embed = new Discord.MessageEmbed()
+            .setTitle(`${randomReplies} ${member.username} 🥺`)
+            .setColor('#FF00A6')
+            .setImage(random.link)
+            message.reply(embed);
+        }
+        pandaFunction();
+    }
+}
