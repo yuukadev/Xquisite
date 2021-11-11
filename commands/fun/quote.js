e0389427f0f649abdc9c81e269e118333e6ddbc4
YuukaDev <vule.amv1@gmail.com>
2021-11-12 00:11:08 +0100

    Update quote.js

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -1,27 +1,24 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
-    name: 'quote',
-    description: "This command sends you random quote",
-    category: 'fun',
-    example: ['!quote'],
-    callback({message}) {
-        let quote = async () => {
-            try {
-                let res = await fetch("https://api.quotable.io/random");
-                let random = await res.json();
+  name: "quote",
+  description: "This command sends you random quote",
+  category: "fun",
+  example: ["!quote"],
+  callback: async ({ message }) => {
+    try {
+      let res = await fetch("https://api.quotable.io/random");
+      let random = await res.json();
 
-                const embed = new Discord.MessageEmbed()
-                    .setAuthor(`📜 Author - ${random.author}`)
-                    .setColor('#FF00A6')
-                    .setDescription(random.content)
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(`📜 Author - ${random.author}`)
+        .setColor("#FF00A6")
+        .setDescription(random.content);
 
-                message.channel.send(embed);
-            }catch(err) {
-                message.channel.send('there is something wrong :(');
-            }
-        }
-        quote();
+      message.channel.send(embed);
+    } catch (err) {
+      message.channel.send("there is something wrong :(");
     }
-}
+  },
+};
