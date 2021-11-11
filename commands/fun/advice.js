0449e383e2c63d9e2502e8821a5fce136f8a879d
YuukaDev <vule.amv1@gmail.com>
2021-11-12 00:11:03 +0100

    Create advice.js

---
+++ commands/fun/advice.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "advice",
+  description: "This command sends you random advice",
+  category: "fun",
+  example: ["!advice"],
+  callback: async ({ message }) => {
+    try {
+      let res = await fetch("https://api.adviceslip.com/advice");
+      let random = await res.json();
+
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(`Here's some advice for - ${message.author.username} 💖`)
+        .setColor("#FF00A6")
+        .setDescription(random.slip.advice);
+
+      message.channel.send(embed);
+    } catch (err) {
+      message.channel.send("there is something wrong :(");
+    }
+  },
+};
