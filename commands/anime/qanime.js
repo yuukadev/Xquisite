eb76ecd63d133e280cce42b6642107411d3386f6
YuukaDev <vule.amv1@gmail.com>
2021-11-12 00:11:00 +0100

    Update qanime.js

--- commands/anime/qanime.js
+++ commands/anime/qanime.js
@@ -1,28 +1,27 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
-    name: 'qanime',
-    aliases: 'quoteanime',
-    description: "This command sends you random anime quote",
-    category: 'anime',
-    example: ['!qanime'],
-    callback({message}) {
-        let animeQuote = async () => {
-            try {
-                let res = await fetch("https://animechan.vercel.app/api/random");
-                let random = await res.json();
+  name: "qanime",
+  aliases: "quoteanime",
+  description: "This command sends you random anime quote",
+  category: "anime",
+  example: ["!qanime"],
+  callback: async ({ message }) => {
+    try {
+      let res = await fetch("https://animechan.vercel.app/api/random");
+      let random = await res.json();
 
-                const embed = new Discord.MessageEmbed()
-                    .setAuthor(`📜 Character - ${random.character} | 📜 Anime - ${random.anime}`)
-                    .setColor('#FF00A6')
-                    .setDescription(random.quote)
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(
+          `📜 Character - ${random.character} | 📜 Anime - ${random.anime}`
+        )
+        .setColor("#FF00A6")
+        .setDescription(random.quote);
 
-                message.channel.send(embed);
-            }catch(err) {
-                message.channel.send('there is something wrong :(');
-            }
-        }
-        animeQuote();
+      message.channel.send(embed);
+    } catch (err) {
+      message.channel.send("there is something wrong :(");
     }
-}
+  },
+};
