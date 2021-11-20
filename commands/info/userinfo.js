d4f4365b0a069b7ffcbb04e61df92df3bb9b6197
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:17:11 +0100

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,27 +1,29 @@
-const Discord = require('discord.js');
-const moment = require('moment');
+const Discord = require("discord.js");
+const moment = require("moment");
 
 module.exports = {
-    name: 'userinfo',
-    description: 'This command sends info about user',
-    category: 'info',
-    example: ['!userinfo'],
-    callback({message}) {
-        const user = message.mentions.users.first() || message.author;
-        const member = message.guild.members.cache.get(user.id);
+  name: "userinfo",
+  description: "This command sends info about user",
+  category: "info",
+  example: ["!userinfo"],
+  callback({ message }) {
+    const user = message.mentions.users.first() || message.author;
+    const member = message.guild.members.cache.get(user.id);
 
-        let embed = new Discord.MessageEmbed()
-        .setTitle(`User Information - ${user.username}`)
-        .setThumbnail(user.displayAvatarURL({ dynamic: true }))
-        .setColor('#FF00A6')
-        .addField(":busts_in_silhouette: Informations", [
-            `• **Name**: ${user.tag}`,
-            `• **User ID**: ${user.id}`,
-            `• **Nickname**: ${member.nickname || 'None'}`,
-            `• **Is Bot**: ${user.bot ? 'True' : 'False'}`,
-            `• **Created At**: ${moment(member.user.createdTimestamp).format('LL LTS')}`,
-            `• **Joined At**: ${moment(member.joinedAt).format('LL LTS')}`,
-        ])
-        message.channel.send(embed)
-    }
-}
+    let embed = new Discord.MessageEmbed()
+      .setTitle(`User Information - ${user.username}`)
+      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+      .setColor("#FF00A6")
+      .addField(":busts_in_silhouette: Informations", [
+        `• **Name**: ${user.tag}`,
+        `• **User ID**: ${user.id}`,
+        `• **Nickname**: ${member.nickname || "None"}`,
+        `• **Is Bot**: ${user.bot ? "True" : "False"}`,
+        `• **Created At**: ${moment(member.user.createdTimestamp).format(
+          "LL LTS"
+        )}`,
+        `• **Joined At**: ${moment(member.joinedAt).format("LL LTS")}`,
+      ]);
+    message.channel.send(embed);
+  },
+};
