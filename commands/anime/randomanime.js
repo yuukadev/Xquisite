8c84be1a506b51cdb0367ca229d07a87b2039f3f
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:17:17 +0100

    Update randomanime.js

--- commands/anime/randomanime.js
+++ commands/anime/randomanime.js
@@ -1,21 +1,21 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
-    name: 'randomanime',
-    aliases: 'ranime',
-    description: 'this command sends a random image of anime',
-    category: 'anime',
-    example: ['!randomanime', /* Or */ '!ranime'],
-    callback({message}) {
-        const randomanime = require('random-anime');
-        const anime = randomanime.anime()
-        let member = message.author;
+  name: "randomanime",
+  aliases: "ranime",
+  description: "this command sends a random image of anime",
+  category: "anime",
+  example: ["!randomanime", /* Or */ "!ranime"],
+  callback({ message }) {
+    const randomanime = require("random-anime");
+    const anime = randomanime.anime();
+    let member = message.author;
 
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`Image for ${member.username}`)
-        .setImage(anime)
-        .setColor("#FF00A6")
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Image for ${member.username}`)
+      .setImage(anime)
+      .setColor("#FF00A6");
 
-        message.channel.send(embed);
-    }
-}
+    message.channel.send(embed);
+  },
+};
