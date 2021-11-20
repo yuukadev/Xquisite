12fca7f96346bf851f770311f421e5d6e8d93de9
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:17:01 +0100

    Update removerole.js

--- commands/moderation/removerole.js
+++ commands/moderation/removerole.js
@@ -1,45 +1,56 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
-    name: 'removerole',
-    description: "This command remove's role for someone",
-    category: 'moderation',
-    example: ['!removerole @member @role'],
-    callback({message, client, args}) {
-        const permission = message.member.hasPermission("MANAGE_ROLES");
-        const member = message.mentions.members.first()
+  name: "removerole",
+  description: "This command remove's role for someone",
+  category: "moderation",
+  example: ["!removerole @member @role"],
+  callback({ message, client, args }) {
+    const permission = message.member.hasPermission("MANAGE_ROLES");
+    const member = message.mentions.members.first();
 
-        if(!permission) {
-            return message.channel.send(`❌ | You don't have permission to use this command`)
-        }
-
-        if(!args[0]) {
-            return message.channel.send(`❌ | Please specify someone`);
-        }
-
-        if(!member) {
-            return message.channel.send(`❌ | Cannot find a member`);
-        }
-
-        if(message.member.roles.highest.position < member.roles.highest.position){
-            return message.channel.send(`❌ | You cannot remove role to user who have higher role than you...`)
-        }
-
-        if(message.member.roles.highest.position > message.guild.members.resolve(client.user).roles.highest.position){
-            return message.channel.send(`❌ | The roles cannot be removed because they are higher than or equal to my highest role`);
-        }
-
-        let role = message.mentions.roles.first()
-
-        if(!role) {
-            return message.channel.send(`❌ | There is no role with that name `);
-        }
-
-        if(!member.roles.cache.has(role.id)) {
-            return message.channel.send(`❌ | That person does not have that role`);
-        }
-
-        member.roles.remove(role);
-        message.channel.send(`:gear: | Role ${role} has been removed from ${member}`);    
+    if (!permission) {
+      return message.channel.send(
+        `❌ | You don't have permission to use this command`
+      );
     }
-}
+
+    if (!args[0]) {
+      return message.channel.send(`❌ | Please specify someone`);
+    }
+
+    if (!member) {
+      return message.channel.send(`❌ | Cannot find a member`);
+    }
+
+    if (message.member.roles.highest.position < member.roles.highest.position) {
+      return message.channel.send(
+        `❌ | You cannot remove role to user who have higher role than you...`
+      );
+    }
+
+    if (
+      message.member.roles.highest.position >
+      message.guild.members.resolve(client.user).roles.highest.position
+    ) {
+      return message.channel.send(
+        `❌ | The roles cannot be removed because they are higher than or equal to my highest role`
+      );
+    }
+
+    let role = message.mentions.roles.first();
+
+    if (!role) {
+      return message.channel.send(`❌ | There is no role with that name `);
+    }
+
+    if (!member.roles.cache.has(role.id)) {
+      return message.channel.send(`❌ | That person does not have that role`);
+    }
+
+    member.roles.remove(role);
+    message.channel.send(
+      `:gear: | Role ${role} has been removed from ${member}`
+    );
+  },
+};
