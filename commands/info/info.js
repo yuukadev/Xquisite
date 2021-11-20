016482d6cebc2d1cacf1ceca2aef8c1651f09847
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:17:14 +0100

    Update info.js

--- commands/info/info.js
+++ commands/info/info.js
@@ -1,26 +1,39 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
-    name: 'info',
-    description: 'This command sends info about bot',
-    category: 'info',
-    example: ['!info'],
-    callback({message}) {
-
+  name: "info",
+  description: "This command sends info about bot",
+  category: "info",
+  example: ["!info"],
+  callback({ message }) {
     const embed = new Discord.MessageEmbed()
-    .setColor('#FF00A6')
-    .setAuthor("Xquisite Info", message.author.avatarURL({dynamic: true}))
-    .setDescription("Xquisite is a friendly bot, made purely out of boredom, the goal of this bot is to improve someone's server experience, for more information look at - `x!help`") 
-    .addFields(
-		{ name: '</Twitter>', value: '[Twitter Profile](https://twitter.com/yuukasuoh)', inline: true },
-		{ name: '</GitHub>', value: '[GitHub Profile](https://github.com/YuukaDev)', inline: true },
-        { name: '</Reddit>', value: '[Reddit Profile](https://www.reddit.com/user/Rintarrou)', inline: true },
-    )
-    .attachFiles('logo.jpg')
-    .setImage('attachment://logo.jpg')
-    .setTimestamp()
-    .setFooter('Made by Yuuka');
+      .setColor("#FF00A6")
+      .setAuthor("Xquisite Info", message.author.avatarURL({ dynamic: true }))
+      .setDescription(
+        "Xquisite is a friendly bot, made purely out of boredom, the goal of this bot is to improve someone's server experience, for more information look at - `x!help`"
+      )
+      .addFields(
+        {
+          name: "</Twitter>",
+          value: "[Twitter Profile](https://twitter.com/yuukasuoh)",
+          inline: true,
+        },
+        {
+          name: "</GitHub>",
+          value: "[GitHub Profile](https://github.com/YuukaDev)",
+          inline: true,
+        },
+        {
+          name: "</Reddit>",
+          value: "[Reddit Profile](https://www.reddit.com/user/Rintarrou)",
+          inline: true,
+        }
+      )
+      .attachFiles("logo.jpg")
+      .setImage("attachment://logo.jpg")
+      .setTimestamp()
+      .setFooter("Made by Yuuka");
 
     message.channel.send(embed);
-    }
-}
+  },
+};
