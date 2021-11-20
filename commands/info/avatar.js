7b99c517fda167ebadc993687431709c0fbe5f68
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:17:15 +0100

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
-    name: 'avatar',
-    description: 'This command sends image of your discord avatar',
-    category: 'info',
-    example: ['!avatar', /* Or */ '!avatar @member'],
-    callback({message}) {
-        let member = message.mentions.users.first() || message.author;
-        let avatar = member.displayAvatarURL({dynamic: true, size: 1024})
+  name: "avatar",
+  description: "This command sends image of your discord avatar",
+  category: "info",
+  example: ["!avatar", /* Or */ "!avatar @member"],
+  callback({ message }) {
+    let member = message.mentions.users.first() || message.author;
+    let avatar = member.displayAvatarURL({ dynamic: true, size: 1024 });
 
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`${member.username}'s avatar`)
-        .setImage(avatar)
-        .setColor('#FF00A6')
-        message.channel.send(embed);
-    }
-}
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`${member.username}'s avatar`)
+      .setImage(avatar)
+      .setColor("#FF00A6");
+    message.channel.send(embed);
+  },
+};
