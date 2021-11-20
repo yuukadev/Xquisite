6b8640a4475decfd701b6b5168f8be80f2b39344
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:17:19 +0100

    Update manga.js

--- commands/anime/manga.js
+++ commands/anime/manga.js
@@ -1,37 +1,44 @@
-const Discord = require('discord.js');
-const Jikan = require('jikan-node');
+const Discord = require("discord.js");
+const Jikan = require("jikan-node");
 const mal = new Jikan();
 
 module.exports = {
-    name: 'manga',
-    aliases: 'mangasearch',
-    description: "This command search for manga you typed",
-    category: 'anime',
-    example: ['!manga @manganame'],
-    callback({message, args}) {
-    
+  name: "manga",
+  aliases: "mangasearch",
+  description: "This command search for manga you typed",
+  category: "anime",
+  example: ["!manga @manganame"],
+  callback({ message, args }) {
     let member = message.author;
-    
-    if(!args[0]) {
-        return message.channel.send('❓ | Please tell me what manga to search');
+
+    if (!args[0]) {
+      return message.channel.send("❓ | Please tell me what manga to search");
     }
 
-    mal.search("manga", args, 'page').then(info => {
-    const embed = new Discord.MessageEmbed() 
-    .setAuthor(`📜 Manga for ${member.username}`)
-    .setThumbnail(`${info.results[0].image_url}`)
-    .setColor('#FF00A6')
-    .setTitle(`${info.results[0].title}`)
-    .setURL(`${info.results[0].url}`)
-    .setDescription(`${info.results[0].synopsis}`)
-    .addFields(
-        { name: '📅 Release Date', value: `${info.results[0].start_date || 'Not yet aired'}`, inline: true}, 
-        { name: '🗓️ End Date', value: `${info.results[0].end_date || 'Airing'}`, inline: true},
-        { name: '✨ Score', value: `${info.results[0].score}`, inline: true},
-        { name: '🖥️ Type', value: `${info.results[0].type}`, inline: true},
-    )
-    .setImage(`${info.results[0].image_url}`)
-    message.channel.send(embed);
-        });
-    }
-}
+    mal.search("manga", args, "page").then((info) => {
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(`📜 Manga for ${member.username}`)
+        .setThumbnail(`${info.results[0].image_url}`)
+        .setColor("#FF00A6")
+        .setTitle(`${info.results[0].title}`)
+        .setURL(`${info.results[0].url}`)
+        .setDescription(`${info.results[0].synopsis}`)
+        .addFields(
+          {
+            name: "📅 Release Date",
+            value: `${info.results[0].start_date || "Not yet aired"}`,
+            inline: true,
+          },
+          {
+            name: "🗓️ End Date",
+            value: `${info.results[0].end_date || "Airing"}`,
+            inline: true,
+          },
+          { name: "✨ Score", value: `${info.results[0].score}`, inline: true },
+          { name: "🖥️ Type", value: `${info.results[0].type}`, inline: true }
+        )
+        .setImage(`${info.results[0].image_url}`);
+      message.channel.send(embed);
+    });
+  },
+};
