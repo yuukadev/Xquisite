715438dda291ac646c4f66e9015e5dda6eefcb9d
YuukaDev <vule.amv1@gmail.com>
2022-01-08 23:30:34 +0100

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -5,41 +5,51 @@ module.exports = {
   name: "serverinfo",
   description: "This command sends info about server",
   category: "info",
-  example: ["!serverinfo"],
-  callback({ client, message, args }) {
+  example: ["x!serverinfo"],
+  callback({ message }) {
     let roles = message.guild.roles.cache
       .sort((a, b) => b.position - a.position)
       .map((role) => role.name);
     let members = message.guild.members.cache;
     let channels = message.guild.channels.cache;
+    let owner = members.find((member) => member.id === message.guild.ownerId);
     let embed = new Discord.MessageEmbed()
       .setTitle(`Server Information - ${message.guild.name}`)
       .setThumbnail(message.guild.iconURL())
       .setColor("#FF00A6")
-      .addField("📂 General Information", [
-        `• **Name**: ${message.guild.name}`,
-        `• **Server ID**: ${message.guild.id}`,
-        `• **Owner**: <@${message.guild.ownerID}>`,
-        `• **Server Created**: ${moment(message.guild.createdTimestamp).format(
-          "LL LTS"
-        )}`,
-      ])
-      .addField("📂 Member Information", [
-        `• **Users**: ${message.guild.memberCount}`,
-        `• **Bots**: ${members.filter((m) => m.user.bot).size}`,
-      ])
+      .addField(
+        "📂 General Information",
+        [
+          `• **Name**: ${message.guild.name}`,
+          `• **Server ID**: ${message.guild.id}`,
+          `• **Owner**: ${owner}`,
+          `• **Server Created**: ${moment(
+            message.guild.createdTimestamp
+          ).format("dddd, MMMM Do YYYY")}`,
+        ].join("\n")
+      )
+      .addField(
+        "📂 Member Information",
+        [
+          `• **Users**: ${message.guild.memberCount}`,
+          `• **Bots**: ${members.filter((m) => m.user.bot).size}`,
+        ].join("\n")
+      )
 
-      .addField("📂 Channel Information", [
-        `• **Channels**: ${message.guild.channels.cache.size}`,
-        `• **Text Channels**: ${
-          channels.filter((ch) => ch.type === "text").size
-        }`,
-        `• **Voice Channels**: ${
-          channels.filter((ch) => ch.type === "voice").size
-        }`,
-      ])
+      .addField(
+        "📂 Channel Information",
+        [
+          `• **Channels**: ${message.guild.channels.cache.size}`,
+          `• **Text Channels**: ${
+            channels.filter((ch) => ch.type === "text").size
+          }`,
+          `• **Voice Channels**: ${
+            channels.filter((ch) => ch.type === "voice").size
+          }`,
+        ].join("\n")
+      )
 
-      .addField("📂 Role Information", [`${roles}`]);
-    message.channel.send(embed);
+      .addField("📂 Role Information", [`${roles}`].join("\n"));
+    message.channel.send({ embeds: [embed] });
   },
 };
